d3c583e2a994ac0d0a1100e274f5447fa6c27f1f
Abhishek Maurya <144514324+abhishekmaurya149@users.noreply.github.com>
2023-09-23 18:43:14 +0530

    add

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+//dotenv configuration
+dotenv.config();
+//rest object
+const app = express();
+
+//midlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use('/api/v1/portfolio', require('./routes/portfolioRoute'))
+
+// port
+const PORT = process.env.PORT || 8080
+
+//listen
+app.listen(PORT, () => {
+    console.log(`Server Running On PORT ${PORT}`);
+});
